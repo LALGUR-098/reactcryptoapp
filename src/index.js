9e55ba70f563d5012c5eaf668c1d78ea19336659
unknown <mamraj09112002@gmail.com>
2023-05-17 10:28:56 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import  { StrictMode } from 'react';
+import {  ColorModeScript } from "@chakra-ui/react";
+
+
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ChakraProvider} from "@chakra-ui/react";
+import ColorModeSwitcher from './ColorModeSwitcher';
+import { theme } from '@chakra-ui/react';
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  <StrictMode>
+    <ColorModeScript />
+    <ChakraProvider theme={theme}>
+      <ColorModeSwitcher />
+      <App />
+    </ChakraProvider>
+  </StrictMode>
+); 
+export const server ="https://api.coingecko.com/api/v3"
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
